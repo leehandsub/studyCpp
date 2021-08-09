6ff56e7981c4f3ccdec78f4172d69d95cc3b071e
leehandsub <35682233+leehandsub@users.noreply.github.com>
2021-08-09 19:21:11 +0900

    Update 숫자의 총 개수.cpp

--- implement/숫자의 총 개수.cpp
+++ implement/숫자의 총 개수.cpp
@@ -20,4 +20,20 @@ int main(void)
 	}
 	printf("%d", sum);
 
-}
+}//난 결국 한바퀴를 숫자만큼 다 돌지만 답은 몇번 더하기만 해주면 끝이라 시간이 훨씬 줄어든다.
+/*
+#include<stdio.h>
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int n, sum=0, cnt=1, digit=9, res=0;
+	scanf("%d", &n);
+	while(sum+digit<n){	
+		sum=sum+digit;
+		res=res+(cnt*digit);
+		cnt++;
+		digit=digit*10;
+	}
+	res=res+((n-sum)*cnt);
+	printf("%d\n", res);
+	return 0;
+}*/
