f6ff356373e6226a769d819033974c5bff547f45
leehandsub <35682233+leehandsub@users.noreply.github.com>
2021-09-05 18:42:35 +0900

    Update 합이 같은 부분집합.cpp

--- dfs,bfs/합이 같은 부분집합.cpp
+++ dfs,bfs/합이 같은 부분집합.cpp
@@ -57,3 +57,33 @@ int main(void)
 	}
 
 }
+//다른 방법으로는 토탈을 구하고 반대껄 뺴면서 확인하면 굳이 배열 2개 안써도 됨...
+/*#include<stdio.h>
+int n, a[11], total = 0;
+bool flag = false;
+void DFS(int L, int sum) {
+	if (sum > (total / 2)) return;
+	if (flag == true) return;
+	if (L == n + 1) {
+		if (sum == (total - sum)) {
+			flag = true;
+		}
+	}
+	else {
+		DFS(L + 1, sum + a[L]);
+		DFS(L + 1, sum);
+	}
+}
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int i;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+		total += a[i];
+	}
+	DFS(1, 0);
+	if (flag) printf("YES\n");
+	else printf("NO\n");
+	return 0;
+}*/
