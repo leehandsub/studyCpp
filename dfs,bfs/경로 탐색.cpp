1452194d9b79d7d3924f3f85f1cf2ea3243411f9
leehandsub <35682233+leehandsub@users.noreply.github.com>
2021-09-07 19:50:31 +0900

    Update 경로 탐색.cpp

--- dfs,bfs/경로 탐색.cpp
+++ dfs,bfs/경로 탐색.cpp
@@ -43,4 +43,50 @@ int main(void)
 	dfs(0);
 	printf("%d", cnt);
 	
+}//2차원 배열 사용
+/*#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int check[30];
+vector <int> a[30];
+int cnt;
+int N;
+void dfs(int x)
+{
+	if (x == N)
+	{
+		cnt++;
+	}
+	else
+	{
+		for (int i = 0; i < a[x].size(); i++)
+		{
+			if (check[a[x][i]]==0)
+			{
+				check[a[x][i]] = 1;
+				dfs(a[x][i]);
+				check[a[x][i]] = 0;
+			}
+		}
+	}
 }
+
+int main(void)
+{
+	int M;
+	int c, d;
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < M; i++)
+	{
+		scanf("%d", &c, &d);
+		a[c].push_back(d);
+	}
+	check[1] = 1;
+	dfs(1);
+}*/
+//vector 사용
