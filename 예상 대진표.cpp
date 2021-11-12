5002025e0e5188a166492591322f20d2669418e1
leehandsub <35682233+leehandsub@users.noreply.github.com>
2021-11-12 14:24:40 +0900

    Create 예상 대진표.cpp

---
+++ 예상 대진표.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    int cnt=0;
+    while(a!=b)
+    {
+        if(a%2==1)
+            a++;
+        if(b%2==1)
+            b++;
+        a/=2;
+        b/=2;
+        cnt++;
+    }
+    return cnt;
+}
