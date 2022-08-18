731beb20f9ee04ba6243cc2957dfc631b275c73c
leehandsub <gustjq7027@naver.com>
2022-08-18 16:15:33 +0900

    swea 문제풀기

---
+++ swexpert/1233.cpp
@@ -0,0 +1,68 @@
+﻿/*
+문제 : swea 사칙연산 유효성 검사
+시간 : 30분
+ide 활용 : o
+질문 보기 : o
+피드백 : 문제를 이해 못해서 질문을 참고해서 풀었다.
+굳이 트리를 안만들고 input으로 들어오는 것으로 판단해서 풀었다.
+n/2==num인 부분에서 scanf를 두번해서 자꾸 이상하게 나왔다.
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int T;
+	int num;
+	char c;
+	int x, y;
+	int n;
+	freopen("input.txt", "r", stdin);
+	//scanf("%d", &T);
+
+	for (int t = 1; t <= 10; t++)
+	{
+		int flag = 1;
+		scanf("%d", &n);
+		for (int i = 0; i < n; i++)
+		{
+			scanf("%d", &num);
+			if (n / 2 >= num)
+			{
+				scanf(" %c", &c);
+				if (c >= '0' && c <= '9')
+				{
+					flag = 0;
+				}
+				if (n / 2 == num && n % 2 == 0)
+					scanf("%d", &x);
+				else
+					scanf("%d %d", &x, &y);
+
+			}
+			else
+			{
+				scanf(" %c", &c);
+				if (c < '0' || c> '9')
+				{
+					flag = 0;
+				}
+			}
+		}
+		printf("#%d %d\n", t, flag);
+	}
+}
---
+++ swexpert/9843.cpp
@@ -0,0 +1,52 @@
+﻿/*
+문제 : swea 촛불 이벤트
+시간 : 40분
+ide 활용 : x
+질문 보기 : o
+피드백 : 등차수열이라 n(n+1)/2을 이용했다.
+n<sqrt(2*k)이므로 sqrt(2*k)구해 소수점 버림을 하고 n(n+1)/2==k인지 확인했다.
+visual에서는 math.h를 안해도 됐는데 swea에서 자꾸 실행이안돼 고민하다 헤더를 추가하니 됐다.
+cin을 첨에 사용하다 scanf를하니 시간초과가 안떳다. 한 20분은 삽질한거 같다.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <list>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+vector<long long> arr;
+
+int main(void)
+{
+	int T;
+	int num;
+	char c;
+	int x, y;
+	//int n,p;
+	long long n;
+	//freopen("input.txt", "r", stdin);
+	scanf("%d", &T);
+
+	for (int t = 1; t <= T; t++)
+	{
+		scanf("%lld", &n);
+
+		long long a = sqrt(n * 2);
+
+		if (a * (a + 1) / 2 == n)
+			printf("#%d %d\n", t, a);
+		else
+			printf("#%d %d\n", t, -1);
+	}
+}
